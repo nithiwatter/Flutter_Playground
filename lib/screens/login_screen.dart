ea73e637b8dd6cd3a011010344afbc314087a1f1
Nithiwat Seesillapachai <nithiwatter@gmail.com>
2020-10-26 16:43:42 +0700

    login page

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  Widget _buildTitle() {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 50),
+      child: Column(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Hello',
+            style: const TextStyle(fontSize: 80, fontWeight: FontWeight.bold),
+          ),
+          Text('Log in to your account',
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold))
+        ],
+      ),
+    );
+  }
+
+  Widget _emailField() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        alignment: Alignment.centerLeft,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(30.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              blurRadius: 20,
+              offset: Offset(0, 2),
+            ),
+          ],
+        ),
+        height: 60.0,
+        child: TextField(
+          keyboardType: TextInputType.emailAddress,
+          style: TextStyle(
+            color: Colors.black,
+          ),
+          decoration: InputDecoration(
+            contentPadding: const EdgeInsets.only(top: 15),
+            border: InputBorder.none,
+            prefixIcon: Icon(
+              Icons.account_circle,
+              color: Colors.black,
+            ),
+            hintText: 'Enter your email',
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _passwordField() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        alignment: Alignment.centerLeft,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(30),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              blurRadius: 20,
+              offset: Offset(0, 2),
+            ),
+          ],
+        ),
+        height: 60.0,
+        child: TextField(
+          obscureText: true,
+          style: TextStyle(
+            color: Colors.black,
+          ),
+          decoration: InputDecoration(
+            contentPadding: const EdgeInsets.only(top: 15),
+            border: InputBorder.none,
+            prefixIcon: Icon(
+              Icons.lock,
+              color: Colors.black,
+            ),
+            hintText: 'Enter your password',
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _forgotYourPassword() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Align(
+        alignment: Alignment.centerRight,
+        child: Text('Forgot your password?',
+            style: TextStyle(
+              color: Colors.grey,
+            )),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          height: double.infinity,
+          width: double.infinity,
+          color: Colors.white,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _buildTitle(),
+              _emailField(),
+              SizedBox(
+                height: 30,
+              ),
+              _passwordField(),
+              SizedBox(
+                height: 20,
+              ),
+              _forgotYourPassword()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
